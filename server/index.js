7b2d40bc41db7f1481d07a4b238db3c56c287e6a
nickwai1 <53497786+nickwai1@users.noreply.github.com>
2021-07-22 15:04:42 -0700

    routing progress

--- server/index.js
+++ server/index.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const axios = require('axios');
 
-
 const app = express();
 const PORT = 3000;
 
 app.use(express.static('client/public'));
+app.use(express.json());
 
 app.listen(PORT, () => {
   console.log(`Server listening on port: ${PORT}`);
--- server/routes/Overview/routes.js
+++ server/routes/Overview/routes.js
@@ -1,7 +1,16 @@
 const app = require('../../index.js');
+const api = require('./api.js');
 
-/*
-app.get('/', (req, res) => {
+// controller page 
 
+app.get('/products/', (req, res) => {
+    console.log('Products', req.body);
+    var{id} = req.body.id;
+    api.getProducts(id)
+        .then((data) => {
+            res.status(200).send(data);
+        })
+        .catch((err) => {
+            res.status(404).send(err);
+        })
 })
-*/
