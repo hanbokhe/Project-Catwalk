1e2c7da4411d65008d8195c2806143dd3f6e9877
nickwai1 <53497786+nickwai1@users.noreply.github.com>
2021-07-22 15:04:42 -0700

    routing progress

--- client/src/components/Overview/Overview.jsx
+++ client/src/components/Overview/Overview.jsx
@@ -1,9 +1,30 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import axios from 'axios';
 
 const Overview = () => {
+    const [products, setProducts] = useState([])
+
+    useEffect(() => {
+        axios.get('/products')
+            .then(res => {
+                console.log(res)
+                setProducts(res.data);
+            })
+            .catch(err => {
+                console.log(err)
+            })
+    })
+
     return (
         <div> 
-            Overview
+            <ul>
+                {
+                    products.map(product => (
+                        <li key={product.id}>{product.title}</li>
+                    ))
+                }
+            </ul>
+
         </div>
     )
 }
--- server/routes/Overview/api.js
+++ server/routes/Overview/api.js
@@ -1,2 +1,17 @@
 const axios = require('axios');
-const TOKEN = require('../config.js');
+const config = require('../config.js');
+
+//model page
+const getProducts = (id) => {
+
+    axios({
+        method: 'get',
+        url: 'https://app-hrsei-api.herokuapp.com/api/fec2/hr-sfo' + id,
+        headers: {'Authorization': `token ${config.TOKEN}`},
+    })
+    .then(res => {
+        console.log(res)
+    })
+}
+
+module.exports = {getProducts};
