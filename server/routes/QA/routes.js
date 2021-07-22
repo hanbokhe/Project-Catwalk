ea53a9e74920c2ef6ae80968e2ad961eb129e49a
hanbokhe <sooyeonk@usc.edu>
2021-07-21 18:22:46 -0700

    Webpack Config Fixed

--- server/routes/QA/routes.js
+++ server/routes/QA/routes.js
@@ -1,7 +1,15 @@
 const app = require('../../index.js');
+const api = require('./api.js');
 
-/*
-app.get('/', (req, res) => {
 
+app.get('/products', (req, res) => {
+  console.log('Related Products', req.body);
+  var {id} = req.body;
+  api.getRelated(id)
+    .then((data) => {
+      res.status(200).send(data);
+    })
+    .catch((err) => {
+      res.status.(404).send(err);
+    })
 })
-*/
