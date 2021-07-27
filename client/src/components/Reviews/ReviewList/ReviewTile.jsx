37fba9dc8b87999d133c8e2a4151ec7836adf4f3
Xing Voong <xingvoong@gmail.com>
2021-07-26 23:03:13 -0700

    render data in review tile dynamicly

--- client/src/components/Reviews/ReviewList/ReviewTile.jsx
+++ client/src/components/Reviews/ReviewList/ReviewTile.jsx
@@ -1,39 +1,72 @@
 import React from 'react';
+import Photo from './Photo.jsx';
 
 class ReviewTile extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      helpfulCount: 0
+      starRating: 4,
+      date: 0,
+      reviewSummary: "abc",
+      reviewBody: "def",
+      photos: [],
+      recommend: true,
+      reviwerName: "username",
+      response: "internal response team",
+      helpfulness: 0
     }
   }
+
+  componentDidMount() {
+    this.setState({
+      starRating: this.props.review.rating,
+      date: this.props.review.date,
+      reviewSummary: this.props.review.summary,
+      reviewBody: this.props.review.body,
+      photos: this.props.review.photos,
+      recommend: this.props.review.recommend,
+      reviwerName: this.props.review.reviewer_name,
+      response: this.props.review.response,
+      helpfulness: this.props.review.helpfulness
+
+    })
+  }
+
+
   render() {
     return (
       <div>
         <h1> Review Tile</h1>
         <div>
-          Star rating
+          rating review: {this.state.starRating}
         </div>
         <div>
-          Date of review
+          Date of review: {this.state.date}
         </div>
         <div>
-          Review Summary
+          Review Summary: {this.state.reviewSummary}
         </div>
         <div>
-          Review Body
+          Review Body: {this.state.reviewBody}
         </div>
         <div>
-          Recommend
+          Review Photo: {this.state.photos.map((photo,index) => <Photo
+              key={index}
+              photo={photo}
+              />
+          )}
         </div>
         <div>
-          Reviewer name
+          Recommend: {this.state.recommend ? "yes" : "no"}
         </div>
         <div>
-          Response to Review
+          Reviewer name: {this.state.reviwerName}
         </div>
         <div>
-          Rating Helpfulness
+          Response to Review: {this.state.response}
+        </div>
+        <div>
+          Rating Helpfulness: {this.state.helpfulness}
         </div>
         <div>
           <button>
