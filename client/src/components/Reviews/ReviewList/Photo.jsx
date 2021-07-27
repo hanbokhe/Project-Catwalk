efed8df19918c9dc344a7462f14ca7b4d2e38615
Xing Voong <xingvoong@gmail.com>
2021-07-26 23:03:13 -0700

    render data in review tile dynamicly

---
+++ client/src/components/Reviews/ReviewList/Photo.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+
+var Photo = ({photo}) => {
+  return (
+    <div>
+      <img className="thumbnail"
+        src={photo.url}
+        alt={photo.id}
+        width="193"
+        height="130"
+      />
+    </div>
+  )
+};
+
+export default Photo;
--- client/src/components/Reviews/ReviewList/ReviewList.jsx
+++ client/src/components/Reviews/ReviewList/ReviewList.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReviewTile from './ReviewTile.jsx';
 import TotalSort from './TotalSort.jsx';
 import WriteReview from './WriteReview.jsx';
+import axios from 'axios';
 
 
 class ReviewList extends React.Component {
@@ -10,12 +11,44 @@ class ReviewList extends React.Component {
     this.state = {
       reviewList : []
     }
+    this.getReviews = this.getReviews.bind(this)
   }
+
+  getReviews(id) {
+    axios({
+      method: 'get',
+      url: 'http://localhost:3000/reviews',
+      params: {
+        product_id: id
+      }
+    })
+      .then((data) => {
+        //console.log(data.data.results),
+        this.setState({
+          reviewList: data.data.results
+        })
+        // console.log("new state", this.state.reviewList)
+      })
+      .catch((err) => {
+        console.log(err)
+      })
+  }
+
+  componentDidMount() {
+    this.getReviews(25192)
+  }
+
   render() {
     return (
       <div> Hello ReviewList
       <TotalSort />
-      <ReviewTile />
+        {this.state.reviewList.map((review, index) =>
+        <ReviewTile
+          key = {index}
+          review = {review}
+        />
+      )}
+
       <WriteReview />
       </div>
     )
