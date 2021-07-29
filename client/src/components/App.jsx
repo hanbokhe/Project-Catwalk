a23da1c431e11e517c7d6ee212adc1df41f01d8b
Xing Voong <xingvoong@gmail.com>
2021-07-29 16:32:08 -0700

    center container

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -3,14 +3,23 @@ import React, {useState} from 'react';
 import RelatedProduct from './RelatedProducts/RelatedProducts.jsx';
 // import QA from './QA/QA.jsx';
 import Reviews from './Reviews/Reviews.jsx';
+import styled from 'styled-components';
+
+const Container = styled.div`
+width: 100%;
+display: flex;
+flex-direction: column;
+align-items: center;
+border: solid 1px;
+`
 
 const App = (props) => {
   const [currentProductId, setCurrentProduct] = useState(25170);
   return (
-    <div>
+    <Container>
       <RelatedProduct currentProductId={currentProductId} />
       <Reviews />
-    </div>
+    </Container>
   )
 }
 
