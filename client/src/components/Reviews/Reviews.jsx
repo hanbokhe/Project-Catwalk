536a10a6aebf0b4ae2a785c2e01d27b6afacbd6d
Xing Voong <xingvoong@gmail.com>
2021-07-29 22:29:05 -0700

    checkpoint

--- client/src/components/Reviews/Reviews.jsx
+++ client/src/components/Reviews/Reviews.jsx
@@ -1,13 +1,20 @@
 import React from 'react';
 import ReviewList from './ReviewList/ReviewList.jsx';
 import RatingBreakdown from './RatingBreakdown/RatingBreakdown.jsx';
+import styled from 'styled-components';
+
+const Container = styled.div`
+width: 70%;
+display: flex;
+flex-direction: column;
+`
 
 function Reviews(props) {
   return (
-    <div>
+    <Container>
       <RatingBreakdown />
       <ReviewList currentProductId={props.currentProductId}/>
-    </div>
+    </Container>
   )
 }
 
