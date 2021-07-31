4c70b0e50b4568d8ddb7f2c9d9fc5e167f824521
Xing Voong <xingvoong@gmail.com>
2021-07-30 18:16:50 -0700

    checkpoint

---
+++ client/src/components/Reviews/RatingBreakdown/Characteristics.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+
+//Size, Width, Comfort, Quality, Length, and Fit
+var Characteristics = ({characteristics}) => {
+  return (
+    <div>
+      <div>
+        Size: characteristics.
+      </div>
+
+      <div>
+
+      </div>
+
+      <div>
+
+      </div>
+
+      <div>
+
+      </div>
+
+      <div>
+
+      </div>
+
+    </div>
+
+  );
+};
+
+export default Characteristics;
