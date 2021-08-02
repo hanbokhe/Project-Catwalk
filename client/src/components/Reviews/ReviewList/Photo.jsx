c14f29db7ed9fc01383438431ebe09cf6debb451
Xing Voong <xingvoong@gmail.com>
2021-08-01 23:58:46 -0700

    completed modal windown

--- client/src/components/Reviews/ReviewList/Photo.jsx
+++ client/src/components/Reviews/ReviewList/Photo.jsx
@@ -1,35 +1,43 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ModalImage from 'react-modal-image';
 import styled from 'styled-components';
-
-const Img = styled.img`
-width: 193px;
-height: 130px;
-object-fit: scale-down;
-`;
-
-// var Photo = ({photo}) => {
-//   console.log(photo);
-//   return (
-//     <ModalImage
-//       small={photo.url}
-//       medium={photo.url}
-//     />
-//   );
-// };
-
-var Photo = ({photo}) => {
-  return (
-    <div>
-      <img className="thumbnail"
-        src={photo.url}
-        alt={photo.id}
-        width="193"
-        height="130"
-      />
-    </div>
-  );
-};
+import Lightbox from 'react-image-lightbox';
+import 'react-image-lightbox/style.css';
 
 
-export default Photo;
+export default class Photo extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      photoIndex: 0,
+      isOpen: false,
+    };
+  }
+
+  render() {
+    const images = [this.props.photo.url];
+    console.log(this.props);
+    const { photoIndex, isOpen } = this.state;
+
+    return (
+      <div>
+        <button type="button" onClick={() => this.setState({ isOpen: true })}>
+          <img className="thumbnail"
+            src={this.props.photo.url}
+            alt={this.props.photo.id}
+            width="193"
+            height="130"
+          />
+        </button>
+
+        {isOpen && (
+          <Lightbox
+            mainSrc={images[photoIndex]}
+            onCloseRequest={() => this.setState({ isOpen: false })}
+          />
+        )}
+      </div>
+    );
+  }
+}
