48ee24052423f24809c11b07309770fcb9069caf
hanbokhe <sooyeonk@usc.edu>
2021-08-02 13:19:57 -0700

    star ratings added

--- client/src/components/RelatedProducts/Card.jsx
+++ client/src/components/RelatedProducts/Card.jsx
@@ -1,7 +1,8 @@
 import React, {useState, useEffect, useContext} from 'react';
+import axios from 'axios';
 import Details from './Details.jsx';
-import styled from 'styled-components';
 import RelatedContext from './RelatedContext.jsx';
+import styled from 'styled-components';
 
 
 const Card_div = styled.div`
@@ -12,12 +13,12 @@ flex-direction: column;
 border: solid 1px lightgray;
 `
 const Img_div = styled.div`
-height: 330px;
+height: 315px;
 width: 250px;
 overflow: hidden;
 `
 const Img = styled.img`
-height: 330px;
+height: 315px;
 width: 250px;
 z-index: -1;
 overflow: hidden;
@@ -50,40 +51,15 @@ color: lightgray;
   color: black;
 }
 `
-/*
-.close,
-.star {
-  position: absolute;
-  padding: 3px;
-  right: 2%;
-  z-index: 1;
-  font-weight: bold;
-}
-
-.close:hover,
-.star:hover {
-  color: rgb(102, 162, 182);
-  cursor: pointer;
-  font-weight: bold;
-}
-*/
 
 const Card = ({style, product, isOutfit}) => {
   const [defaultItem, setDefault] = useState(style.results[0]);
   const [photo, setPhoto] = useState(null);
+  const [average, setAverage] = useState(0);
   const [loaded, setLoaded] = useState(false);
   const {addOutfit, deleteOutfit, setModalInfo, setImgClicked} = useContext(RelatedContext);
 
   const inputRef = React.createRef();
-
-  useEffect(() => {
-    getDefault()
-  }, [style]);
-
-  useEffect(() => {
-    getPhoto()
-  }, [defaultItem]);
-
 
   const getDefault = () => {
     for(var i = 0; i < style.results.length; i++) {
@@ -104,6 +80,46 @@ const Card = ({style, product, isOutfit}) => {
       }
     }
   }
+
+  const getAvg = (product_id) => {
+    axios({
+      method: 'GET',
+      url: 'http://localhost:3000/reviews/meta',
+      params: {
+        product_id: product_id
+      }
+    })
+      .then(({data}) => {
+        var ratings = data.ratings;
+        var count = 0;
+        var total = 0;
+
+        for (var key in ratings) {
+          count += parseInt(ratings[key]);
+          total += (parseInt(key) * ratings[key]);
+        }
+        var avg = Math.round(total / count * 10) / 10;
+        console.log(total);
+        if(avg) {
+          setAverage(avg);
+        }
+      })
+      .catch((err) => console.log(err));
+  }
+
+
+  //lifecycle methods
+
+  useEffect(() => {
+    getDefault()
+    getAvg(style.product_id)
+  }, [style]);
+
+  useEffect(() => {
+    getPhoto()
+  }, [defaultItem]);
+
+  //click handlers
 
   const addClick = (e) => {
     e.preventDefault();
@@ -136,7 +152,7 @@ const Card = ({style, product, isOutfit}) => {
             : null
         }
         </Img_div>
-        <Details defaultStyle={defaultItem} product={product}/>
+        <Details defaultStyle={defaultItem} product={product} average={average}/>
       </Card_div>
     </React.Fragment>
   )
