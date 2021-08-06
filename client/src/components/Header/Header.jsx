cd35b5d6c764c26bb256c60a5648d297a9abe6d2
hanbokhe <sooyeonk@usc.edu>
2021-08-05 20:47:50 -0700

    header fixed

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -4,7 +4,7 @@ import styled from 'styled-components';
 const Header_div = styled.div`
 background-color: black;
 height: 80px;
-width: auto;
+width: 100%;
 display: flex;
 flex-direction: row;
 justify-content: space-between;
