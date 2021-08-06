1c6830ba1f6bc58c0fce74342df16ed961a84a83
hanbokhe <sooyeonk@usc.edu>
2021-08-06 09:45:48 -0700

    update

--- client/public/client_src_components_RelatedProducts_CardImg_jsx.bundle.js
+++ client/public/client_src_components_RelatedProducts_CardImg_jsx.bundle.js
@@ -1,3 +1,4 @@
+"use strict";
 /*
  * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
  * This devtool is neither made for production nor for readable output files.
@@ -14,7 +15,6 @@
   \***********************************************************/
 /***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
 
-"use strict";
 eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"default\": () => (__WEBPACK_DEFAULT_EXPORT__)\n/* harmony export */ });\n/* harmony import */ var _babel_runtime_helpers_taggedTemplateLiteral__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! @babel/runtime/helpers/taggedTemplateLiteral */ \"./node_modules/@babel/runtime/helpers/esm/taggedTemplateLiteral.js\");\n/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! react */ \"./node_modules/react/index.js\");\n/* harmony import */ var styled_components__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! styled-components */ \"./node_modules/styled-components/dist/styled-components.browser.esm.js\");\n\n\nvar _templateObject;\n\n\n\nvar Img = styled_components__WEBPACK_IMPORTED_MODULE_2__.default.img(_templateObject || (_templateObject = (0,_babel_runtime_helpers_taggedTemplateLiteral__WEBPACK_IMPORTED_MODULE_0__.default)([\"\\nheight: 315px;\\nwidth: 250px;\\nz-index: -1;\\noverflow: hidden;\\nobject-fit: cover;\\n\"])));\n\nvar CardImg = function CardImg(_ref) {\n  var photo = _ref.photo,\n      imgClick = _ref.imgClick,\n      description = _ref.description;\n  return /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_1__.createElement(Img, {\n    src: photo,\n    onClick: imgClick,\n    alt: description\n  });\n};\n\n/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (CardImg);\n\n//# sourceURL=webpack://project-catwalk/./client/src/components/RelatedProducts/CardImg.jsx?");
 
 /***/ })
